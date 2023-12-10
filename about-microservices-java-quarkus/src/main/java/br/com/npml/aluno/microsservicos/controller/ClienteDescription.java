b502b021ce396edf07cf507d678d0abd274accf5
npalleta <palleta.sp@gmail.com>
2023-12-10 17:35:17 -0300

    update project - setting swagger UI #3

--- about-microservices-java-quarkus/src/main/java/br/com/npml/aluno/microsservicos/controller/ClienteDescription.java
+++ about-microservices-java-quarkus/src/main/java/br/com/npml/aluno/microsservicos/controller/ClienteDescription.java
@@ -8,25 +8,27 @@ import org.eclipse.microprofile.openapi.annotations.info.License;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 @OpenAPIDefinition(
-        tags = {
-                @Tag(name = "infnet", description = "Microsserviços - Base de Estudo."),
-                @Tag(name = "cliente", description = "API de Cliente para o estudo sobre microsserviços com Quarkus.")
-        },
-        info = @Info(
-                title = "API Cliente - Coleção com dados de cadastro de clientes",
-                version = "1.0.0-BETA",
-                contact = @Contact(
-                name = "Niky Lima",
-                url = "https://github.com/npmltech/About-Microservices-With-Quarkus",
-                email = "devqasp@gmail.com"
+    tags = {
+        @Tag(name = "infnet", description = "Microsserviços - Base de Estudo."),
+        @Tag(name = "cliente", description = "API de Cliente para o estudo sobre microsserviços com Quarkus.")
+    },
+    info = @Info(
+        title = "API Cliente - Coleção com dados de cadastro de clientes",
+        version = "1.0.0-BETA",
+        contact = @Contact(
+        name = "Niky Lima",
+        url = "https://github.com/npmltech/About-Microservices-With-Quarkus",
+        email = "devqasp@gmail.com"
         ),
         license = @License(
-                name = "Apache 2.0",
-                url = "https:www.apache.org/licenses/LICENSE-2.0.html")
+            name = "Apache 2.0",
+            url = "https:www.apache.org/licenses/LICENSE-2.0.html"
         )
-        /* servers = {
-                @Server(url = "http:localhost:8082")
-        } */
+    )
 )
 public class ClienteDescription extends Application {
 }
+
+/* servers = {
+   @Server(url = "http:localhost:8082")
+} */
